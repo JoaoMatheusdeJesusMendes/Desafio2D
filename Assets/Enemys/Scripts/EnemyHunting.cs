4cf2591899b8782026eeae5479f5ba95b140474d
JoaoMatheusdeJesusMendes <111393781+JoaoMatheusdeJesusMendes@users.noreply.github.com>
2023-03-23 19:10:07 -0300

    Atualizações no codigo

--- Assets/Enemys/Scripts/EnemyHunting.cs
+++ Assets/Enemys/Scripts/EnemyHunting.cs
@@ -43,17 +43,50 @@ public class EnemyHunting : MonoBehaviour
     {
         if(!startHunting)
         return;
-        
-        //determina a translação do inimigo
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        /*
         //gira o inimigo caso ele não esteja olhando para o player
-        if(player.position.x < rb.position.x && enemyfollow.forRight)
+        if(player.transform.localPosition.x < rb.transform.localPosition.x && enemyfollow.forRight)
         {
             enemyfollow.Inverts();
         }
-        else if(player.position.x > rb.position.x && !enemyfollow.forRight)
+        else if(player.transform.localPosition.x > rb.transform.localPosition.x && !enemyfollow.forRight)
         {
             enemyfollow.Inverts();
+        }*/
+        //função que gira o player
+        Flip(GetAxis());
+        //determina a velocidade do inimigo
+        rb.velocity = new Vector2(GetAxis() * speed, rb.velocity.y);
+        //transform.Translate(transform.right * speed * Time.deltaTime);
+    }
+
+    //função para ver se o player está a direita ou a esquerda do player
+    private int GetAxis()
+    {
+        if(player.transform.position.x < transform.position.x)
+            return -1;
+            
+
+        if(player.transform.position.x > transform.position.x)
+            return 1;
+        
+        return 0;
+    }
+    
+    //função que rotaciona o player
+    private void Flip(float horizontal)
+    {
+        if(horizontal > 0 && !enemyfollow.forRight)
+        {
+            Debug.Log("AQUI");
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            enemyfollow.forRight = !enemyfollow.forRight;
+        }
+        if(horizontal < 0 && enemyfollow.forRight)
+        {   
+            Debug.Log("OOOOOOOOOOOOOO");
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            enemyfollow.forRight = !enemyfollow.forRight;
         }
     }
 }
